9b32583c1fa070887b4f908bbc84cadde5827d07
Yoshinori SEINO <pek9dkl@hotmail.com>
2016-10-20 01:14:45 +0900

    initial commit

---
+++ WpfApplication2/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string text_ = "Hoge";
+        public string Text
+        {
+            get
+            {
+                return text_;
+            }
+            set
+            {
+                text_ = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        BlockingCollection<int> queue_ = new BlockingCollection<int>();
+        Task<int> WaitEvent10Async()
+        {
+            return Task<int>.Run(() =>
+            {
+                return queue_.Take();
+            });
+        }
+
+        async internal void Process()
+        {
+            using (var r = new Resource())
+            {
+                Text = $"{DateTime.Now}"; // ボタン押下直後に時刻表示
+
+                var t = WaitEvent10Async(); // Event "10" を待つスレッドを用意する
+
+                // 処理を開始する.処理が発行するイベントを受信するリスナーを同時に登録できる。
+                Action<int> monitor = new Action<int>((ev) =>
+                {
+                    if (ev != 10) { return; }
+                    queue_.Add(ev);
+                });
+                var a = await r.StartProcWithMonitor(monitor);
+
+                Text = $"{a}"; // 1秒後に1を表示
+
+                var b = await t; // Event "10" を受信するまでメインスレッドは別のメッセージを処理する
+                r.UnregisterMonitor(monitor);
+
+                Text = $"{b}"; // 10秒後に10を表示
+            }
+        }
+
+    }
+}
---
+++ WpfApplication2/Resource.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    class Resource : IDisposable
+    {
+        Timer timer_ = null;
+
+        internal Resource()
+        {
+            Console.WriteLine($"new {nameof(Resource)}");
+        }
+
+        public void Dispose()
+        {
+            timer_.Dispose();
+            Console.WriteLine($"Dispose {nameof(Resource)}");
+        }
+
+        public static volatile Int32 counter_ = 0;
+
+        private event Action<int> listener_ = null;
+
+        internal Task<int> StartProcWithMonitor(Action<int> listener)
+        {
+            if (listener != null)
+            {
+                listener_ = listener;
+            }
+
+            return Task<int>.Run(() =>
+            {
+                // イベント発行スレッド開始
+                timer_ = new Timer(
+                    (o) => {
+                        counter_ = (Int32)counter_ + 1;
+
+                        // TODO: delegateを登録できるようにする。
+                        listener?.Invoke(counter_);
+                        //queue_.Add(counter_);
+                    },
+                    null,
+                    TimeSpan.FromMilliseconds(0),
+                    TimeSpan.FromMilliseconds(1000)
+                    );
+
+                // 時間がかかる処理
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                return counter_;
+            });
+        }
+
+        internal void UnregisterMonitor(Action<int> listener)
+        {
+            if (listener != null)
+            {
+                listener_ -= listener;
+            }
+        }
+    }
+}
